036ba8bfdbf2a2fab5b8698cdb334e494b95b6d9
Nickolai <lindeman072@mail.ru>
2023-11-24 21:02:33 +0400

    редактирование header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,10 @@ import logo from "../../images/logo.svg";
 export default function Header() {
   return (
     <header className="header">
+      <div className="header__container">
+        <h2 className="header__nav header__nav_active">Трекер</h2>
+        <h2 className="header__nav">Подборки</h2>
+      </div>
       <img src={logo} className="header__logo" alt="логотип" />
     </header>
   );
