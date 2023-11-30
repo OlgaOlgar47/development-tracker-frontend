f8314b4bd240cda5d75ad694f2a84ed518ea6bf2
Nickolai <lindeman072@mail.ru>
2023-11-30 18:05:05 +0400

    fix constant2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,8 +15,8 @@ import ManagerProgectHover from "../../src/images/prodject-manager-hover.svg";
 import ContentManagerHover from "../../src/images/kontent0manager-hover.svg";
 import FrontendHover from "../../src/images/frontend developer-hover.svg";
 
+
 export const cards = [
-<<<<<<< HEAD
   {
     name: "Графический дизайнер",
     count: "10 навыков",
@@ -66,37 +66,3 @@ export const cards = [
     imageHover: FrontendHover,
   },
 ];
-=======
-  {name: "Графический дизайнер", count: "10 навыков", image: Desiner, imageHover: DesinerHover},
-  {name: "Топ 5 навыков в дизайне в 2023 году", count: "5 навыков", image: Top5, imageHover: Top5hover},
-  {name: "Аналитик данных", count: "12 навыков", image: Analitic, imageHover: AnaliticHover},
-  {name: "Дизайнер интерфейсов", count: "23 навыка", image: DesinerInerfase, imageHover: DesinerInerfaseHover},
-  {name: "Инженер по тестированию", count: "15 навыков", image: Tester, imageHover: TesterHover},
-  {name: "Менеджер проектов", count: "18 навыков", image: ManagerProgect, imageHover: ManagerProgectHover},
-  {name: "Контент менеджер", count: "8 навыков", image: ContentManager, imageHover: ContentManagerHover},
-  {name: "Фронтенд-разработчик", count: "14 навыков", image: Frontend, imageHover: FrontendHover},
-  ]  
-
-
-  export const skillsData=[
-    { name: "HTML", percentage: 0 },
-    { name: "CSS", percentage: 0 },
-    { name: "JavaScript", percentage: 0 },
-    { name: "Python", percentage: 0 },
-    { name: "React", percentage: 0 },
-    { name: "Node.js", percentage: 0 },
-    { name: "SQL", percentage: 0 },
-    { name: "Vue.js", percentage: 0 },
-    { name: "Angular", percentage: 0 },
-    { name: "Композиция и сетки", percentage: 80 },
-    { name: "UX-копирайтинг", percentage: 20 },
-    { name: "Анимация", percentage: 40 },
-    { name: "Исследования", percentage: 80 },
-    { name: "Гайдлайны iOS", percentage: 100 },
-    { name: "Вайрфреймы", percentage: 20 },
-    { name: "Анализ ЦА", percentage: 20 },
-    { name: "Гипотезы", percentage: 60 },
-    { name: "dobe Photoshop", percentage: 80 },
-  ]
-  
->>>>>>> 4e04a45fc3d21dbd41b522033f1bcce60525fbb9
