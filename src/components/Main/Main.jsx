3234245afbf11c90d80d3ea4019e3dc388b8f86c
Olga Averianova <olgaolgar47@yandex.ru>
2023-11-30 16:56:28 -0300

    feat: переход на редактирование навыка ура

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -34,7 +34,27 @@ export default function Main({
         <Route path="/collections" element={<Collections collectionsData={collectionsData}
           />} />
         <Route path="/collections/skills" element={<Skills handleAddSkill={handleAddSkill}/>} />
-        <Route path="/skill-editor" element={<SkillEditor />} />
+        <Route path="/skill-editor/:skillId" element={<SkillEditor skillsData={[
+              { id: 0, name: "HTML", percentage: 0, rate: 1, notes: "kakaka" },
+              { id: 1, name: "HTML", percentage: 0, rate: 1, notes: "kakaka" },
+              { id: 2, name: "CSS", percentage: 0 },
+              { id: 3, name: "JavaScript", percentage: 0 },
+              { id: 4, name: "Python", percentage: 0 },
+              { id: 5, name: "React", percentage: 0 },
+              { id: 6, name: "Node.js", percentage: 0 },
+              { id: 7, name: "SQL", percentage: 0 },
+              { id: 8, name: "Vue.js", percentage: 0 },
+              { id: 9, name: "Angular", percentage: 0 },
+              { id: 10, name: "Композиция и сетки", percentage: 80 },
+              { id: 11, name: "UX-копирайтинг", percentage: 20 },
+              { id: 12, name: "Анимация", percentage: 40 },
+              { id: 13, name: "Исследования", percentage: 80 },
+              { id: 14, name: "Гайдлайны iOS", percentage: 100 },
+              { id: 15, name: "Вайрфреймы", percentage: 20 },
+              { id: 16, name: "Анализ ЦА", percentage: 20 },
+              { id: 17, name: "Гипотезы", percentage: 60 },
+              { id: 18, name: "dobe Photoshop", percentage: 80 },
+            ]}/>} />
       </Routes>
     </main>
   );
