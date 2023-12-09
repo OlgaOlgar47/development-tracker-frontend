a1fa4d3cd6d1a72c0ffcf9fc430881996f11e784
Olga Averianova <olgaolgar47@yandex.ru>
2023-12-09 10:46:31 -0300

    feat: const Token in Api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,11 @@
 export const BASE_URL = "http://localhost:8000";
+export const TOKEN = "4fec03972264016ae2c6c0070b62f4abe0acace6";
+
+const headers = {
+  authorization: `Token ${TOKEN}`,
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
 
 const getResponseData = (res) => {
   if (!res.ok) {
@@ -10,135 +17,73 @@ const getResponseData = (res) => {
 export const getUserData = () => {
   return fetch(`${BASE_URL}/api/v1/userData/`, {
     method: "GET",
-    headers: {
-      authorization: "Token 4fec03972264016ae2c6c0070b62f4abe0acace6",
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => {
-      return getResponseData(res);
-    })
-    .then((data) => data);
+    headers,
+  }).then(getResponseData);
 };
 
 export const getSkills = () => {
   return fetch(`${BASE_URL}/api/v1/skills/`, {
     method: "GET",
-    headers: {
-      authorization: "Token 4fec03972264016ae2c6c0070b62f4abe0acace6",
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => {
-      return getResponseData(res);
-    })
-    .then((data) => data);
+    headers,
+  }).then(getResponseData);
 };
+
 
 export const getCollections = () => {
   return fetch(`${BASE_URL}/api/v1/collections/`, {
     method: "GET",
-    headers: {
-      authorization: "Token 4fec03972264016ae2c6c0070b62f4abe0acace6",
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => {
-      return getResponseData(res);
-    })
-    .then((data) => data);
+    headers,
+  }).then(getResponseData);
 };
 
 export const getCourses = () => {
   return fetch(`${BASE_URL}/api/v1/recommended-courses-tracker/`, {
     method: "GET",
-    headers: {
-      authorization: "Token 4fec03972264016ae2c6c0070b62f4abe0acace6",
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => {
-      return getResponseData(res);
-    })
-    .then((data) => data);
+    headers,
+  }).then(getResponseData);
 };
 
 
 export const getCoursesForCollection = (id) => {
   return fetch(`${BASE_URL}/api/v1/recommended-courses-collection/${id}/`, {
     method: "GET",
-    headers: {
-      authorization: "Token 4fec03972264016ae2c6c0070b62f4abe0acace6",
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => {
-      return getResponseData(res);
-    })
-    .then((data) => data);
+    headers,
+  }).then(getResponseData);
 };
 
 export const getCourseForSkillEditor = (id) => {
   return fetch(`${BASE_URL}/api/v1/recommended-courses-skill/${id}/`, {
     method: "GET",
-    headers: {
-      authorization: "Token 4fec03972264016ae2c6c0070b62f4abe0acace6",
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => {
-      return getResponseData(res);
-    })
-    .then((data) => data);
+    headers,
+  }).then(getResponseData);
 };
 
 export const addSkill = (data) => {
   return fetch(`${BASE_URL}/api/v1/skills/`, {
     method: "POST",
-    headers: {
-      authorization: "Token 4fec03972264016ae2c6c0070b62f4abe0acace6",
-      "Content-Type": "application/json",
-    },
+    headers,
     body: JSON.stringify({
       name: data,
     }),
-  }).then((res) => {
-    return getResponseData(res);
-  });
+  }).then(getResponseData);
 };
 
 export const editSkill = (values) => {
   return fetch(`${BASE_URL}/api/v1/skills/${values.id}/`, {
     method: "PATCH",
-    headers: {
-      authorization: "Token 4fec03972264016ae2c6c0070b62f4abe0acace6",
-      "Content-Type": "application/json",
-    },
+    headers,
     body: JSON.stringify({
       name: values.name,
       rate: values.rate,
       notes: values.notes,
     }),
-  })
-    .then((res) => {
-      return getResponseData(res);
-    })
+  }).then(getResponseData);
 };
+
 
 export const deleteSkill = (id) => {
   return fetch(`${BASE_URL}/api/v1/skills/${id}/`, {
     method: "DELETE",
-    headers: {
-      authorization: "Token 4fec03972264016ae2c6c0070b62f4abe0acace6",
-      "Content-Type": "application/json",
-    },
-  }).then((res) => {
-    return getResponseData(res);
-  });
+    headers,
+  }).then(getResponseData);
 };
